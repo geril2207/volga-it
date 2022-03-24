7a5f47468595c2fc95f87df2e44a48d659db944b
Gergert Ilya <gergert.ilya@mail.ru>
2022-03-24 18:43:05 +0300

    init first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FirstPage } from 'components/Pages'
 import { Header, StartPage } from './components'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div className="app">
       <Header />
       <div className="app_body">
-        <StartPage />
+        <FirstPage />
       </div>
     </div>
   )
---
+++ src/components/Pages/First/FirstPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './FirstPage.module.css'
+
+const FirstPage = () => {
+  return (
+    <div className='main_content_wrapper'>
+      <h4>You are looking for</h4>
+      <div>
+        <div>
+          <img src="" alt="" />
+          Women's Styles
+        </div>
+        <div>
+          <img src="" alt="" />
+          Men's Styles
+        </div>
+      </div>
+      <div>I'd like to see both</div>
+    </div>
+  )
+}
+
+export default FirstPage
